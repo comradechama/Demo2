989b613ef6c411342eda4db4c3092e1b7b4bb85f
santia <sachin21890@gmail.com>
2023-01-24 06:42:24 +0530

    updated pom

--- src/test/java/test1/simplePrint.java
+++ src/test/java/test1/simplePrint.java
@@ -1,8 +1,12 @@
 package test1;
 
-public class simplePrint {
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
+public class simplePrint {
+     
+	
+	@Test
+	public void print () {
 		// TODO Auto-generated method stub
 		
 		System.out.println("This for jenkin test only");

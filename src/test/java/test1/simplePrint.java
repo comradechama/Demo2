316636c48f53cc36c35123afe5854ef95a801b24
santia <sachin21890@gmail.com>
2023-01-24 06:01:10 +0530

    test

---
+++ src/test/java/test1/simplePrint.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class simplePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This for jenkin test only");
+
+	}
+
+}
